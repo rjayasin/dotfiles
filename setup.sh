d09543580db9d1a2950dc769ac51a98da42a3b40
rjayasin <rjayasin@users.noreply.github.com>
2018-07-31 20:12:11 -0700

    Sane setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Don't forget to run as root!
+set -e
+set -o pipefail
+
+cd
+apt update || true
+apt -y upgrade
+apt install -y \
+	git \
+	htop \
+	python \
+	python3-pip \
+	silversearcher-ag \
+	tmux
+
+apt autoremove
+apt autoclean
+apt clean
